49d04b688a9b555997e3e3437d82ad437892bf50
cfos3120 <cfos3120@uni.sydney.edu>
2023-06-19 17:31:25 +1000

    fix loss function to 2pi

--- training_utils/loss_functions.py
+++ training_utils/loss_functions.py
@@ -307,7 +307,7 @@ def PINO_loss3d_decider(model_input, model_output, model_val, forcing_type, nu, 
         #print('Performing FDM Vorticity ALl losses MSE')
         x2 = torch.tensor(np.linspace(0, 2*np.pi, S+1)[:-1], dtype=torch.float).reshape(1, S).repeat(S, 1)
         forcing = -4 * (torch.cos(4*(x2))).reshape(1,S,S,1).repeat(B, 1, 1, T-2).to(device)
-        Dw1, Dw2, __, __ = FDM_NS_vorticity_v2(model_output, nu=nu, t_interval=t_interval)
+        Dw1, Dw2, __, __ = FDM_NS_vorticity_v2(model_output, nu=nu, t_interval=t_interval, L = 2*np.pi)
 
         lploss = LpLoss(size_average=True)
         loss_w = lploss(Dw1, forcing) #lploss(Dw1, forcing) #F.mse_loss(Dw1, forcing)
